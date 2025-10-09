d43aed53b2e5c62bfb08a4d69bd8e575239d36b3
stanlys96 <stanlyskwok@gmail.com>
2025-10-09 17:35:54 +0700

    wlao

--- src/pages/ManagePage.tsx
+++ src/pages/ManagePage.tsx
@@ -1,3 +1,4 @@
+import { IoStarOutline } from "react-icons/io5";
 import Header from "../components/Header";
 import { PersonStandingIcon } from "lucide-react";
 
@@ -15,6 +16,15 @@ export const ManagePage = () => {
           </div>
         }
       />
+      <section className="flex justify-center text-white relative bg-gradient-to-br from-surface via-background to-dark-900 py-10 h-[90vh] w-[100vw]">
+        <div className="absolute inset-0 bg-[url('https://images.pexels.com/photos/1254736/pexels-photo-1254736.jpeg?auto=compress&cs=tinysrgb&w=1600')] bg-cover bg-center opacity-10" />
+        <div className="flex w-full h-full flex-row justify-center">
+          <div className="flex gap-x-2 items-center h-fit cursor-pointer">
+            <IoStarOutline color="white" size={20} />
+            <p className="text-[20px]">Featured</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
